3288ca4139ef98c59eb9914fc6abfb19119c61cf
pengfei <pengfeiye1994@gmail.com>
2017-10-04 13:59:35 -0700

    Officially Finish

--- scripts/app.js
+++ scripts/app.js
@@ -13,14 +13,21 @@ $(document).ready(function() {
 var map;
 var locations = []
 function initMap() {
-    var city = { lat: 37.78, lng: -122.44};
+    var city = { lat: 0, lng: 0};
     var map = new google.maps.Map(document.getElementById('map'), {
         center:city,
         zoom: 1
     });
+    var icon = {
+    url: "images/earthquake.png", // url
+    scaledSize: new google.maps.Size(20, 20), // scaled size // anchor
+};
+
+
     var markers = locations.map(function(object, i) {
         return new google.maps.Marker({
             position: object,
+            icon: icon,
             map: map
         });
     });
